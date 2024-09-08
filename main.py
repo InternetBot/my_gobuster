ef6aac661c9ea484d1c6420262ea093f2917eb8a
Calvin-Caleb <91896578+InternetBot@users.noreply.github.com>
2024-09-08 15:20:37 -0400

    added basic functions

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 """
 Just trying to make my own gobuster in python
+attemp to brute force dir
 :3
 """
-print ("Specify Targets IP or Url :3 :  ")
-target = input()
+
+#might add a thread pull executor later.
+
+import requests
 
 #reading the file 
-#sigh for some reason having issues with leading zeros and decimal literal reading into a file (will fix later)
 def read_file(filename):
-    with open('filename', 'r') as f:
-        commondir = f.readlines()
+    with open(filename, 'r') as f:
+        commondir = [line.strip() for line in f.readlines()]
     return commondir
- 
+
+target = input("Specify Targets IP or Url :3 :  ")
+
 filename = 'wordweblist.txt'
 commondir = read_file(filename)
-print(commondir)
+counter = 0
+# print(commondir)
+
+print("Please wait this mughty take a while :P")
+
+for dir in commondir:
+    target_addr = f"{target}/{dir}"
+    response = requests.get(target_addr)
+    if response.status_code == 200:
+        print (f"{target_addr} - Success")
+        counter += 1
+    else:
+        pass # does nothing 
+
+print(f"Total directories found: ", {counter})
+
